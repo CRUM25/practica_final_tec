f28c672e58ae349a869fa6b2a2de84ef581db4a1
CRUM25 <carlurumor@gmail.com>
2025-11-13 22:54:31 -0600

    Update movie_app.py

--- movie_app.py
+++ movie_app.py
@@ -1,8 +1,8 @@
 
 import streamlit as st
 import pandas as pd
-from google.cloud import firestore
-from google.oauth2 import service_account
+#from google.cloud import firestore
+#from google.oauth2 import service_account
 import json
 
 key_dict = json.loads(st.secrets["textkey"])
